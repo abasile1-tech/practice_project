7737a071a0f820d6e654e0f81323b011115e3ac5
John <ashton.basile@gmail.com>
2022-02-28 13:38:30 +0100

    Practiced some simple input/output

---
+++ io_practice/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Please type your name: ";
+	string userInput = "";
+	cin >> userInput;
+	cout << "Your name is: " << userInput << endl;
+	return 0;
+}
